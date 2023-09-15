094d450d35281a08b57578e78186a7080699f0ef
MSI\gn01792218 <gn01792218@hotmail.com>
2023-09-15 18:45:57 +0800

    [feature] add mobile section1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'title':'#004187',
+        'acent':"#F8CB6D"
+      },
+      fontFamily: {
+        notosnscjktcblack: ["NotoSansCJKtc-Black"],
+        notosnscjktcbold: ["NotoSansCJKtc-Bold"],
+        notosnscjktcdemilight: ["NotoSansCJKtc-Demilight"],
+        notosnscjktclight: ["NotoSansCJKtc-Light"],
+        notosnscjktcMedium: ["NotoSansCJKtc-Medium"],
+        notosnscjktcRegular: ["NotoSansCJKtc-Regular"],
+        notosnscjktcThin: ["NotoSansCJKtc-Thin"],
+      }
+    },
   },
   plugins: [],
 }
